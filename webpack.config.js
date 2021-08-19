bdf64d96afcec55615254343dab6ece82dea55de
Mark DeLoura <mdeloura@satori.org>
2021-08-19 11:51:39 -0700

    Working JS test

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		static: {
+			directory: './dist'
+		}
+	},
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+};
